74f1f71b3f629a039940912c19c5727c0830622d
shahinahmed6057 <mdshahinahmed605701@gmail.com>
2023-03-14 18:48:08 +0600

    Completed Checkbox Functionality

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import { Controller, useForm } from "react-hook-form";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
+import { Form, Input, Button, Checkbox } from "semantic-ui-react";
+
+function App() {
+  const formSchema = Joi.object({
+    firstName: Joi.string()
+      .required()
+      // .label("firstName")
+      .messages({
+        "string.empty": "First name can not be empty.",
+      })
+      .min(3)
+      .max(10),
+    lastName: Joi.string()
+      .required()
+      .messages({
+        "string.empty": "Last name can not be empty",
+      })
+      .min(5)
+      .max(15),
+    checkbox: Joi.boolean(),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    clearErrors,
+  } = useForm({
+    mode: "onSubmit",
+    shouldFocusError: true,
+    reValidateMode: "onChange",
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      checkbox: false,
+    },
+    resolver: joiResolver(formSchema),
+  });
+  // console.log(errors);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleSetValue = (key, value) => {
+    setValue(key, value);
+    clearErrors(key);
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="firstName"
+          render={({ field: { value, onChange } }) => (
+            <>
+              <Input
+                label="Enter Your Name"
+                placeholder="Enter Name"
+                value={value}
+                onChange={(e, { value }) => {
+                  onChange(value);
+                }}
+              />
+              {errors.firstName && <p>{errors.firstName.message}</p>}
+            </>
+          )}
+        />
+        <br></br>
+        <Controller
+          control={control}
+          name="lastName"
+          render={({ field: { value, onChange } }) => (
+            <>
+              <Input
+                label="Enter Your Name"
+                placeholder="Enter Name"
+                value={value}
+                onChange={(e, { value }) => {
+                  onChange(value);
+                }}
+              />
+              {errors.lastName && <p>{errors.lastName.message}</p>}
+            </>
+          )}
+        />
+        <br></br>
+        <br></br>
+        <Controller
+          control={control}
+          name="checkbox"
+          render={({ field: { value, onChange } }) => (
+            <Checkbox
+              value={value}
+              onChange={(e, { value }) => {
+                onChange(!value);
+              }}
+            />
+          )}
+        />
+        <br></br>
+        <Button
+          type="button"
+          onClick={() => handleSetValue("firstName", "Shahin")}
+        >
+          Set Value
+        </Button>
+        <Button
+          type="button"
+          onClick={() => handleSetValue("lastName", "Ahmed")}
+        >
+          Set Value
+        </Button>
+        <Button primary type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
